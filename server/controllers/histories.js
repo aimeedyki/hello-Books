c5da9e5743f4dbba79c0a9a4437b85cd1fdd514d
Amarachi <akuwudike.a@gmail.com>
2017-08-08 04:14:28 +0100

    creates route to borrow and return books

--- server/controllers/histories.js
+++ server/controllers/histories.js
@@ -1,48 +1,77 @@
-const history = require('../models').History;
+import {History} from '../models';
+import {User} from '../models';
+import {Book} from '../models';
 
-module.exports = {
+export default {
   //user borrows a book and creates a history record
-  create(req, res) {
-    return history
-      .create({
-        title: req.body.title,
-        borrowedDate: req.body.borrowedDate,
-        return:req.body.return,
-        userId: req.params.userId,
-        bookId: req.body.bookId,  
+  produce (req, res) {
+    return Book.findById(req.body.bookId)
+      .then(Book => {
+        if (!Book) {
+          return res.status(404).send({error: 'Book does not exist'});
+        }
+        if (Book.quantity === 0) {
+          return res.status(200).send({message: 'This book is out of stock! Please try again later.'});
+        }
+
+        return History.findOne({
+          where: {
+            bookId: req.body.bookId
+          }
         })
-      .then(history => res.status(201).send(history))
-      .catch(error => console.log(error.message)); 
+          .then((History) => {
+            History.create({
+              borrowedDate: req.body.borrowedDate,
+              return: req.body.return,
+              userId: req.params.userId,
+              bookId: req.body.bookId,
+            });
+          })
+          .then(Book => {
+            Book.update({
+              quantity: (Book.quantity - 1),
+            });
+          });
+      })
+      .then(History => res.status(201).send(History))
+      .catch(error => res.status(400).send(error.message));
   },
-  //returns the book by updating the history with return date
+
+  // returns the book by updating the history with return date
   modify(req, res) {
-    history.update({
-     title: req.body.title,
-        borrowed: req.body.borrowed,
-          returnDate: req.body.returnDate,
-          returned: req.body.returned,
+    History.update({
+      returnDate: req.body.returnDate,
+      return: req.body.returned,
+    },
+    { where: { userId: req.params.userId, bookId: req.body.bookId },
+      returning: true,
+      plain: true
+    })
+      .then(Book => {
+        Book.update({
+          quantity: (Book.quantity + 1),
+        });
+      })
+      .then(History => res.status(200).send(History))
+      .catch(error => res.status(400).send(error.message));
   },
-    { where: { userId: req.params.userId, title:req.body.title } 
-  })
-      .then(history => res.status(201).send("book returned"))
-      .catch(error => console.log(error.message)); 
-  },
-   //displays user history
+
+  //displays user history
   list(req, res) {
 
+    return History
+      .all({ where: { userId: req.params.userId }})
+
+      .then(histories => res.status(200).send(histories))
+      .catch(error => res.status(400).send(error.message));
+  },
+
+  // displays books not returned
+  returned(req, res) {
     return history
-    .all({ where: { userId: req.params.userId }})
+      .all({ where: { userId: req.params.userId, return: req.query.return}})
 
-    .then(histories => res.status(200).send(histories))
-    .catch(error => res.status(400).send(error.message));
-},
-//display books not neturned
-returned(req, res) {
-    return history
-    .all({ where: { userId: req.params.userId, returned: req.query.returned}})
-
-    .then(histories => res.status(200).send(histories))
-    .catch(error => res.status(400).send(error.message));
-},
-
-}
+      .then(histories => res.status(200).send(histories))
+      .catch(error => res.status(400).send(error.message));
+  },
+};
--- server/models/history.js
+++ server/models/history.js
@@ -1,23 +1,22 @@
 'use strict';
-module.exports = (sequelize, DataTypes)=> {
+export default (sequelize, DataTypes)=> {
   const History = sequelize.define('History', {
     borrowedDate: DataTypes.DATE,
     returnedDate: DataTypes.DATE,
     return: DataTypes.BOOLEAN,
     userId: DataTypes.INTEGER,
     bookId: DataTypes.INTEGER,
-    }, {
+  }, {
     classMethods: {
       associate: (models) => {
         History.belongsTo(models.Book, {
           foreignKey: 'bookId',
-          onDelete: 'CASCADE',
-      });
-      History.belongsTo(models.User, {
+        });
+        History.belongsTo(models.User, {
           foreignKey: 'userId',
           onDelete: 'CASCADE',
-      });
-    }
+        });
+      }
     }
   });
   return History;
