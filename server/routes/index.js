966865a43f719eef3dc23a353e13bb5442e297a5
Amarachi <akuwudike.a@gmail.com>
2017-08-15 00:37:33 +0100

    create endpoint for displaying books by categories

--- server/routes/index.js
+++ server/routes/index.js
@@ -41,14 +41,17 @@ app.post('/api/v1/category', authentication.verifyUser, authentication.verifyAdm
 // route for displaying all categories
 app.get('/api/v1/category', authentication.verifyUser, authentication.verifyAdmin, categoriesController.list);
 
+//route for displaying all the books by categories
+app.get('/api/v1/category/:id', authentication.verifyUser, categoriesController.display)
+
 // route for adding a book
 app.post('/api/v1/books', authentication.verifyUser, authentication.verifyAdmin, booksController.addBook );
 
 // route for modifying book information
 app.put('/api/v1/books/:id', authentication.verifyUser, authentication.verifyAdmin, booksController.modify);
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to booksville',
-}));
+//app.get('*', (req, res) => res.status(200).send({
+//  message: 'Welcome to booksville',
+//}));
 
 export default app;
