e5c68cc3c8c66d5ca5ae0b36880cb97ed1b8fac0
Amarachi <akuwudike.a@gmail.com>
2017-08-18 16:46:43 +0100

    modify models index

--- server/models/index.js
+++ server/models/index.js
@@ -3,9 +3,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.json')[env];
 const db = {};
-
+console.log('+++++++++++++++++++++++++++',  config);
+console.log('============================================', env)
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
