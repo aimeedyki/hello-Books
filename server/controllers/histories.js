1829ee422bc5a3347cbe9412c731cf8540fabc70
aimeedyki <akuwudike.a@gmail.com>
2017-10-09 23:53:07 +0100

    change app.js location

--- server/controllers/histories.js
+++ server/controllers/histories.js
@@ -16,9 +16,6 @@ export default {
           return res.status(404)
             .send({ message: 'This book is out of stock!' });
         }
-        History.findOne({
-          where: { userId: req.params.userId, bookId: req.body.bookId }
-        });
         User.find({ where: { id: req.params.userId } })
           .then((user) => {
             if (user.borrowCount === user.max) {
@@ -27,23 +24,25 @@ export default {
             }
             User.update({
               borrowCount: (user.borrowCount + 1),
-            }, { where: { id: req.params.userId } });
-            History.create({
-              expectedDate: new Date(Date.now() +
-                (user.max * 24 * 60 * 60 * 1000)),
-              returned: false,
-              userId: req.params.userId,
-              bookId: req.body.bookId,
-            }, { returning: true, plain: true });
-            Book.update({
-              quantity: (book.quantity - 1),
-            }, { where: { id: req.body.bookId } });
-            Notification.create({
-              userId: req.params.userId,
-              bookId: req.body.bookId,
-              action: 'Borrowed',
-            });
-            res.status(201).send(history);
+            }, { where: { id: req.params.userId } })
+              .then((history) => {
+                History.create({
+                  expectedDate: new Date(Date.now() +
+                    (user.max * 24 * 60 * 60 * 1000)),
+                  returned: false,
+                  userId: req.params.userId,
+                  bookId: req.body.bookId,
+                }, { returning: true, plain: true });
+                Book.update({
+                  quantity: (book.quantity - 1),
+                }, { where: { id: req.body.bookId } });
+                Notification.create({
+                  userId: req.params.userId,
+                  bookId: req.body.bookId,
+                  action: 'Borrowed',
+                });
+                res.status(201).send(history);
+              });
           });
       })
       .catch(error => res.status(400).send(error.message));
